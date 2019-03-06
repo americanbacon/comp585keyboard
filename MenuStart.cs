79ba850a0d5e1536cae5d9778560516b593bde5c
bethany39 <45519082+bethany39@users.noreply.github.com>
2019-03-06 14:01:02 -0500

    Add files via upload

---
+++ MenuStart.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuStart : MonoBehaviour
+{
+    public void changeMenuScreen(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+}
